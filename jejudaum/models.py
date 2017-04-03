2ffd1d052a733b277a32e5fba44e96c4e8d03373
leeikjeun <dldlrwjs12@gmail.com>
2017-04-03 22:10:07 +0900

    test

--- jejudaum/models.py
+++ jejudaum/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Restaurant(models.Model):
+    r_gruop = models.CharField(max_length = 20)
+    r_name = models.CharField(max_length = 20)
+    r_call = models.CharField(max_length = 20)
+
+    def __str__(self):
+        return "%s %s" %(self.r_name, self.r_call)
+
+
+class Menu(models.Model):
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
+    m_name = models.CharField(max_length = 20)
+    m_price = models.CharField(max_length = 10)
+
+    def __str__(self):
+        return "%s %s %s" %(self.restaurant, self.m_name, self.m_price)
