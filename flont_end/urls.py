355bb7dd72d0c8949dbd4ac1b5bae503cda4e803
leeikjeun <dldlrwjs12@gmail.com>
2017-04-04 11:30:40 +0900

    make whateat

--- flont_end/urls.py
+++ flont_end/urls.py
@@ -20,4 +20,5 @@ from jejudaum import views
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', views.index , name = "index"),
+    url(r'^whateat/$', views.whatEat , name = "whateat"),
 ]
